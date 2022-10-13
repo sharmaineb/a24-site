9575e1a05413d959db0be15850782a94c54a7cac
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-10-12 21:07:46 -0700

    started js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left:0,
+        behavior: 'smooth'
+    })
+})
