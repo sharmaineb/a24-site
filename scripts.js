7684ecbd6efd37b034a4d6084867138d495ad941
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-10-14 15:42:06 -0700

    finished rough version

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 // Scroll to Top 
-const button = document.querySelector('button');
-button.addEventListener('click', () => {
+const topButton = document.getElementsByClassName('topButton');
+topButton.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         left:0,
@@ -8,174 +8,51 @@ button.addEventListener('click', () => {
     })
 })
 
-// ------------------------------------------------
+// The function and show all columns
+filterSelection("all") 
+function filterSelection(c) {
+  let x, i;
+  x = document.getElementsByClassName("column");
+  if (c == "all") c = "";
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i++) {
+    removeProduction(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addProductuion(x[i], "show");
+  }
+}
 
-// filterSelection('all')
-// function filterSelection(e) {
-//     const x = document.getElementsByClassName('card');
-//     if (e == 'all') e = '';
-//     for (i = 0; i < x.length; i++) {
-//         removeClass(x[i], "show");
-//         if (x[i].className.indexOf(e) > -1) addClass(x[i], "show")
-//     }
-// }
+// Show the elements that are filtered
+function addProductuion(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
 
-// function addClass(element, name) {
-//     var i, arr1, arr2;
-//     arr1 = element.className.split(' ');
-//     arr2 = name.split(' ');
-//     for (i = 0; i < arr2.length; i++) {
-//         if (arr1.indexOf(arr2[i] == -1)) {
-//             element.className += ' ' + arr2[i]
-//         }
-//     }
-// }
+// Hide the elements that are not selected
+function removeProduction(element, name) {
+  let i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1); 
+    }
+  }
+  element.className = arr1.join(" ");
+}
 
-// function removeClass(element, name) {
-//     let i, array1, array2;
-//     array1 = element.className.split(" ");
-//     array2 = name.split(" ");
-//     for (i = 0; i < array2.length; i++) {
-//       while (array1.indexOf(array2[i]) > -1) {
-//         array1.splice(array1.indexOf(array2[i]), 1); 
-//       }
-//     }
-//     element.className = array1.join(" ");
-//   }
-
-// const buttonContainer = document.getElementById("buttonContainer");
-// const btns = buttonContainer.getElementsByClassName("btn");
-// for (const i = 0; i < btns.length; i++) {
-//   btns[i].addEventListener("click", function() {
-//     const current = document.getElementsByClassName("active");
-//     current[0].className = current[0].className.replace(" active", "");
-//     this.className += " active";
-//   });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const films = document.querySelectorAll(".card")
-// films.forEach((film)=> {
-//     console.log(film)
-//     console.log(film.dataset.name)
-//     film.style.display = "flex"
-// })
-
-// function search_film() {
-//     let input = document.getElementById('searchbar').value
-//     input = input.toLowerCase()
-//     let x = document.getElementsByClassName('card')
-
-//     for (i = 0; i < x.length; i++) {
-//         if (!x[i].innerHTML.toLowerCase(). includes(input)) {
-//             x[i].style.display="none"
-//         }
-//         else {
-//             x[i].style.display="list-item"
-//         }
-//     }
-// }
-
-// const films = [
-//     {film: 'Causeway'},
-//     {film: 'The Whale'},
-//     {film: 'Irma Vep'},
-//     {film: 'Ramy'},
-//     {film: 'Minari'},
-//     {film: 'Moonlight'},
-//     {film: 'Zola'},
-//     {film: 'Everything Everywhere All At Once'},
-//     {film: 'Euphoria'},
-//     {film: 'Ex Machina'},
-//     {film: 'Uncut Gems'},
-//     {film: 'The Green Knight'},
-// ]
-
-// productionElement.addEventListener("event type"), () => {
-
-// }
-
-// const searchInput = document.querySelector('.input')
-// searchInput.addEventListener("input", (e) => {
-//     let value = e.target.value 
-//     if (value && value.trim().length > 0){
-//         value = value.trim().toLowerCase()
-//      } else {
-
-//     }    
-// }
-
-// const clearButton = document.getElementById('clear')
-// clearButton.addEventListener("click", () => {
-
-// })
-
-// function setList(results) {
-//     for (const film of results){
-//         const resultFilm = document.createElement('li')
-//         resultFilm.classList.add('result-film')
-//         const text = document.createTextNode(film.dataset.name)
-//         resultFilm.appendChild(text)
-//         list.appendChild(resultFilm)
-//     }
-// }
-
-// searchInput.addEventListener("input", (e) => {
-//     let value = e.target.value
-//     if (value && value.trim().length > 0) {
-//         value = value.trim().toLowerCase()
-//         setList(films.filter(film => {
-//             return film.name.includes(value)
-//         }))
-//     }
-// })
-
-// function clearList() {
-//     while (list.firstChild){
-//         list.removeChiled(list.firstChild)
-//     }
-// }
-
-// searchInput.addEventListener("input", (e) => {
-//     let value = e.target.value
-//     if (value && value.trim().length > 0){
-//         value = value.trim().toLowerCase()
-//     } else {
-//         clearList()
-//     }
-// }
-
-// clearSearchButton.addEventListener("click", () => {
-//     clearList()
-// })
-
-// function noResults() {
-//     const error = document.createElement('li')
-//     error.classList.add('error-message')
-//     const text = document.createTextNode("No Results Found. Please Try Again!")
-//     error.appendChild(text)
-//     list.appendChild(error)
-// }
-
-// function setList(results){
-//     clearList()
-//     for (const film of results){
-//         const resultFilm = document.createElement('li')
-//         resultFilm.classList.add('result-film')
-//     }
-//     if (results.length === 0){
-//         noResults()
-//     }
-// })
-
+// Add active class to the current button
+const btnContainer = document.getElementById("myBtnContainer");
+const btns = btnContainer.getElementsByClassName("btn");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
