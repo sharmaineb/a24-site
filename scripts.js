cd2ca30d6b4b9d47f7c17540ee0d36cb5c0c888b
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-10-13 21:55:55 -0700

    continued working on project

--- scripts.js
+++ scripts.js
@@ -10,27 +10,82 @@ button.addEventListener('click', () => {
 
 // ------------------------------------------------
 
-const films = document.querySelectorAll(".card")
-films.forEach((film)=> {
-    console.log(film)
-    console.log(film.dataset.name)
-    film.style.display = "flex"
-})
+// filterSelection('all')
+// function filterSelection(e) {
+//     const x = document.getElementsByClassName('card');
+//     if (e == 'all') e = '';
+//     for (i = 0; i < x.length; i++) {
+//         removeClass(x[i], "show");
+//         if (x[i].className.indexOf(e) > -1) addClass(x[i], "show")
+//     }
+// }
 
-function search_film() {
-    let input = document.getElementById('searchbar').value
-    input = input.toLowerCase()
-    let x = document.getElementsByClassName('film')
+// function addClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(' ');
+//     arr2 = name.split(' ');
+//     for (i = 0; i < arr2.length; i++) {
+//         if (arr1.indexOf(arr2[i] == -1)) {
+//             element.className += ' ' + arr2[i]
+//         }
+//     }
+// }
 
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase(). includes(input)) {
-            x[i].style.display="none"
-        }
-        else {
-            x[i].style.display="list-item"
-        }
-    }
-}
+// function removeClass(element, name) {
+//     let i, array1, array2;
+//     array1 = element.className.split(" ");
+//     array2 = name.split(" ");
+//     for (i = 0; i < array2.length; i++) {
+//       while (array1.indexOf(array2[i]) > -1) {
+//         array1.splice(array1.indexOf(array2[i]), 1); 
+//       }
+//     }
+//     element.className = array1.join(" ");
+//   }
+
+// const buttonContainer = document.getElementById("buttonContainer");
+// const btns = buttonContainer.getElementsByClassName("btn");
+// for (const i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     const current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const films = document.querySelectorAll(".card")
+// films.forEach((film)=> {
+//     console.log(film)
+//     console.log(film.dataset.name)
+//     film.style.display = "flex"
+// })
+
+// function search_film() {
+//     let input = document.getElementById('searchbar').value
+//     input = input.toLowerCase()
+//     let x = document.getElementsByClassName('card')
+
+//     for (i = 0; i < x.length; i++) {
+//         if (!x[i].innerHTML.toLowerCase(). includes(input)) {
+//             x[i].style.display="none"
+//         }
+//         else {
+//             x[i].style.display="list-item"
+//         }
+//     }
+// }
 
 // const films = [
 //     {film: 'Causeway'},
